d48d2b51214bdbe4be332cf72d4f9b012332fb45
Simon <simon@dancingcloudservices.com>
2023-09-12 10:06:14 -0600

    Final

---
+++ src/main/java/generics/Examples.java
@@ -0,0 +1,18 @@
+package generics;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Examples {
+  public static void main(String[] args) {
+    ArrayList al = new ArrayList();
+    al.add("Fred");
+    al.add(LocalDate.now());
+    System.out.println(al);
+
+//    Object name = al.get(0);
+    String name = (String)al.get(1);
+
+    System.out.println();
+  }
+}
---
+++ src/main/java/looping/Simple.java
@@ -0,0 +1,25 @@
+package looping;
+
+public class Simple {
+  public static void main(String[] args) {
+    int x = 3;
+    while (x > 0) {
+      System.out.println("x is " + x);
+//      x = x - 1;
+//      x -= 1;
+      x--; // ++x x++ --x also...
+    }
+
+    System.out.println("---------------------");
+    for (int y = 3; y > 0; y--) {
+      System.out.println("y is " + y);
+    } // y now out of scope :)
+
+    int y = 0;
+
+    System.out.println("---------------------");
+    do {
+      System.out.println("value of y is " + y);
+    } while (y > 0);
+  }
+}
